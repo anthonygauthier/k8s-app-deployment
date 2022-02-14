5d8f7cce5e11bbdc4837748eae16ec9d84a13ed7
Anthony Gauthier <antho325@hotmail.com>
2022-02-14 11:25:46 -0500

    Modified main.tf to remove leading white spaces in JSON

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,19 +22,19 @@ resource "aws_iam_role" "eks_role" {
   name = "eks-cluster"
 
   assume_role_policy = <<POLICY
-    {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Effect": "Allow",
-                "Principal": {
-                    "Service": "eks.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole"
-            }
-        ]
-    }
-    POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "eks.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+        }
+    ]
+}
+POLICY
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
