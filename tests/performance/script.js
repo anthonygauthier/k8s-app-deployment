1893a9c8dc88209549b3b973d5873a1af51b4668
Anthony Gauthier <antho325@hotmail.com>
2022-02-16 00:20:48 -0500

    Fixed kubectl command, fixed perf tests

--- tests/performance/script.js
+++ tests/performance/script.js
@@ -1,10 +1,23 @@
 import http from 'k6/http';
-import { check } from 'k6';
+import { check, sleep } from 'k6';
 
-export default function() {
-    let res = http.get(`https://${process.env.API_URL}.com/login/`);
+export const options = {
+    duration: '1m',
+    vus: 1,
+};
+
+function testCase() {
+    let res = http.get(`https://${process.env.API_URL}/automation`);
+
+    if(res.error_code) {
+        sleep(5)
+        testCase();
+    }
+    
     check(res, {
         'status code 200':                  (r) => r.status === 200,
         'body contains correct message':    (r) => r.body.includes("Automate all the things!"),
     });
 }
+
+export default testCase()
